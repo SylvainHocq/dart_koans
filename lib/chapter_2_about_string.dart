3dcd9c626e58a68a8d8a3620bbbe955bb6d8fdd2
shocq <hocq.sylvain@michelin.com>
2023-06-13 14:56:53 +0200

    Complete solutions

--- lib/chapter_2_about_string.dart
+++ lib/chapter_2_about_string.dart
@@ -70,29 +70,29 @@ Partner!
 
   void koan_escaping_quotes_at_the_end_of_a_triple_quoted_string() {
     var string = """Hello "world\"""";
-    assert(your_answer == string);
+    assert('Hello "world"' == string);
   }
 
   void koan_plus_concatenates_strings() {
     var string = "Hello" + ", " + "World";
-    assert(your_answer == string);
+    assert("Hello, World" == string);
   }
 
   void koan_$_is_a_better_concatination() {
     var a = "Hello";
     var b = "World";
 
-    assert(your_answer == "$a, $b");
+    assert("Hello, World" == "$a, $b");
   }
 
   void koan_dollar_plus_curly_brackets_allows_attributes_selection() {
     var string = "Concatenation is great!";
-    assert(your_answer == "${string.length}");
+    assert("23" == "${string.length}");
   }
 
   void koan_adjacent_strings_are_concatenated_automatically() {
     var string = "Hello" ", " "World";
-    assert(your_answer == string);
+    assert("Hello, World" == string);
   }
 
   void koan_plus_will_not_modify_the_original_strings() {
@@ -100,15 +100,15 @@ Partner!
     var bar = "World";
     var string = foo + bar;
 
-    assert(your_answer == foo);
-    assert(your_answer == bar);
+    assert("Hello, " == foo);
+    assert("World" == bar);
   }
 
   void koan_plus_equals_will_append_to_the_end_of_the_string() {
     var foo = "Hello, ";
     var bar = "World";
     foo += bar;
-    assert(your_answer == foo);
+    assert("Hello, World" == foo);
   }
 
   void koan_plus_equals_also_leaves_original_string_unmodified() {
@@ -116,6 +116,6 @@ Partner!
     var foo = original;
     var bar = "World";
     foo += bar;
-    assert(your_answer == original);
+    assert("Hello, " == original);
   }
 }
